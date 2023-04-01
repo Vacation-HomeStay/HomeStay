8b1ba57476b1abd1ab1516e3ac11aca79d77f51b
Jeremy Kurian <jeremykurian68@gmail.com>
2023-04-01 07:22:03 -0400

    working carousel feature

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,49 +2,55 @@ import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { auth } from "../firebase";
+import CarouselCards from "../components/CarouselCards";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.replace("Login");
-      })
-      .catch((error) => alert(error.message));
-  };
+	const handleSignOut = () => {
+		auth.signOut()
+			.then(() => {
+				navigation.replace("Login");
+			})
+			.catch((error) => alert(error.message));
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <Text>{auth.currentUser.uid}</Text>
-      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<SafeAreaView>
+				<CarouselCards />
+			</SafeAreaView>
+
+			<Text>Email: {auth.currentUser?.email}</Text>
+			<Text>{auth.currentUser.uid}</Text>
+
+			<TouchableOpacity onPress={handleSignOut} style={styles.button}>
+				<Text style={styles.buttonText}>Sign out</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	button: {
+		backgroundColor: "#0782F9",
+		width: "60%",
+		padding: 15,
+		borderRadius: 10,
+		alignItems: "center",
+		marginTop: 40,
+	},
+	buttonText: {
+		color: "white",
+		fontWeight: "700",
+		fontSize: 16,
+	},
 });
