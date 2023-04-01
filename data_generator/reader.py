5e397ded0bf0c89e17aed617bdfd9cd492abb24e
Ari Kamat <arijkamat@gmail.com>
2023-04-01 03:16:27 -0400

    Demographics are now stored with the UID as the document ID

---
+++ data_generator/reader.py
@@ -0,0 +1,21 @@
+import names
+from random import randint
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+cred = credentials.Certificate("../firebase/homestay.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection('users')
+
+doc_ref = doc_ref.document('HsDmCd3XbKVBDkC8orLlH0ZbfMq1')
+doc = doc_ref.get()
+
+# Check if the document exists
+if doc.exists:
+    print(f"Document data: {doc.to_dict()}")
+    print(f"Document ID: {doc.id}")
+else:
+    print("The document does not exist")
