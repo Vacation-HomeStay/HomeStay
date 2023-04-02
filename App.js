9c5e6d995ee0e4dd966c89da0a583b4a60d829cf
Jeremy Kurian <jeremykurian68@gmail.com>
2023-04-02 08:08:01 -0400

    final touches

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { LogBox } from "react-native"
+
+LogBox.ignoreAllLogs(true)
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
